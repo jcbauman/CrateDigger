4c505f8a43e0e015ade9415ce18f0228f144f7c2
Jack "Rocko" Bauman <jcbauman@Ucsc.edu>
2018-10-26 07:18:02 -0700

    implemented swipecards

--- app/src/main/java/com/example/rocko/cratedigger/MainActivity.java
+++ app/src/main/java/com/example/rocko/cratedigger/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.rocko.cratedigger;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        al = new ArrayList<>();
+        al.add("Superfly");
+        al.add("Best Friend");
+        al.add("Fly Like an Eagle");
+        al.add("Respect");
+        al.add("Chicken Little Theme Song");
+        al.add("Musicawi Sikt");
+        al.add("Too Late");
+        al.add("Move On Up");
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+
+
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+//    static void makeToast(Context ctx, String s){
+//        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+//    }
+//
+//
+//    @OnClick(R.id.right)
+//    public void right() {
+//        /**
+//         * Trigger the right event manually.
+//         */
+//        flingContainer.getTopCardListener().selectRight();
+//    }
+//
+//    @OnClick(R.id.left)
+//    public void left() {
+//        flingContainer.getTopCardListener().selectLeft();
+//    }
 }
